3c51d9e73f294323988a8840d7bd64d7d53e4bec
Adam Lusted <94024260+adamlusted-wow@users.noreply.github.com>
2023-12-02 19:02:24 +1100

    stuff

--- 2023/day-2.ts
+++ 2023/day-2.ts
@@ -1,52 +1,23 @@
-
-
-const guards = new Map([
-    ['blue', 14],
-    ['green', 13],
-    ['red', 12]
-]);
-
-
 const puzzleInput = await Deno.readTextFile("./2023/day-2-input.txt");
 const lines = puzzleInput.split('\n');
 
-lines.pop();
+const dice = new Set([
+    [{rx: /[0-9]{0,4}\sblue/g, max: 14}],
+    [{rx: /[0-9]{0,4}\sgreen/g, max: 13}],
+    [{rx: /[0-9]{0,4}\sred/g, max: 12}]
+])
 
-const blueRegex = /[0-9]{0,4}\sblue/g
-const greenRegex = /[0-9]{0,4}\sgreen/g
-const redRegex = /[0-9]{0,4}\sred/g
-const gameRegex = /Game\s[0-9]{0,3}/
-
-
-const validGames = lines.filter(line => {
+const result = lines.filter(line => {
     let validGame = true;
     const throws = line.split('; ');
     throws.forEach(hand => {
-        const redCubes = hand.match(redRegex) ?? ['0'];
-        const blueCubes = hand.match(blueRegex) ?? ['0'];
-        const greenCubes = hand.match(greenRegex) ?? ['0'];
-
-        const redCount = redCubes.reduce((acc, cur) => {
-            return acc + parseInt(cur.split(' ')[0]);
-        }, 0);
-        const blueCount = blueCubes.reduce((acc, cur) => {
-            return acc + parseInt(cur.split(' ')[0]);
-        }, 0);
-        const greenCount = greenCubes.reduce((acc, cur) => {
-            return acc + parseInt(cur.split(' ')[0]);
-        }, 0);
-
-        if(redCount > guards.get('red')) validGame = false;
-        if(blueCount > guards.get('blue')) validGame = false;
-        if(greenCount > guards.get('green')) validGame = false;
+        [...dice].forEach(([{rx, max}]) => {
+            const count = (hand.match(rx) ?? ['0']).reduce((acc, cur) => acc + Number(cur.split(' ')[0]), 0);
+            if (count > max) validGame = false;
+        });
     })
-
     return validGame;
-});
+}).reduce((acc, cur) => acc + Number(cur.match(/Game\s[0-9]{0,3}/)[0].split(' ')[1]), 0);
 
-const result = validGames.reduce((acc, cur) => {
-    const gameNumber = cur.match(gameRegex)[0].split(' ')[1];
-    return acc + Number(gameNumber);
-}, 0);
-
+// 2818
 console.log(result);
