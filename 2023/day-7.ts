672bfcd4601dc242a9eb424ef9b1a8774ec798a6
Adam Lusted <94024260+adamlusted-wow@users.noreply.github.com>
2023-12-07 20:28:06 +1100

    stuff

--- 2023/day-7.ts
+++ 2023/day-7.ts
@@ -4,6 +4,23 @@ KK677 28
 KTJJT 220
 QQQJA 483`
 
+enum CardValue {
+    One = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six  = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = "T",
+    Jack = "J",
+    Queen = "Q",
+    King = "K",
+    Ace = "A",
+}
+
 enum HandType {
     HighCard,
     OnePair,
@@ -13,6 +30,45 @@ enum HandType {
     FourOfAKind,
     FiveOfAKind,
 }
+
+const hashTableOfSorts = new Map();
+
+const faceValue = [
+    CardValue.One,
+    CardValue.Two,
+    CardValue.Three,
+    CardValue.Four,
+    CardValue.Five,
+    CardValue.Six,
+    CardValue.Seven,
+    CardValue.Eight,
+    CardValue.Nine,
+    CardValue.Ten,
+    CardValue.Jack,
+    CardValue.Queen,
+    CardValue.King,
+    CardValue.Ace,
+];
+const type = [
+    HandType.HighCard,
+    HandType.OnePair,
+    HandType.TwoPair,
+    HandType.ThreeOfAKind,
+    HandType.FullHouse,
+    HandType.FourOfAKind,
+    HandType.FiveOfAKind,
+];
+
+for (let i = 0,score = 0; i < faceValue.length; i++) {
+    const faceValueType = faceValue[i];
+    for (let j = 0; j < type.length; j++,score++) {
+        const typeCard = type[j];
+        hashTableOfSorts.set(`${typeCard}-${faceValueType}`,score);
+    }
+}
+
+console.log(hashTableOfSorts);
+
 
 const games = puzzleInput.split('\n').map(rawHand => {
     const [cardGroup, bid] = rawHand.split(/\s/);
@@ -27,29 +83,23 @@ const games = puzzleInput.split('\n').map(rawHand => {
 const classifiedHands = games.map(({cardGroup, bid}) => {
     const what = Object.groupBy([...cardGroup], c => c);
     const typeGroups = Object.entries(what);
-    const typeAmounts = typeGroups.map(([c,cs]) => cs.length);
-    const playedType = Math.max(...typeAmounts);
+    const typeAmounts = typeGroups.map(([c,cs]) => [c,cs.length]);
 
-
-
+    const score = Math.max(...typeAmounts.map(c => hashTableOfSorts.get(`${c[1]}-${c[0]}`)));
 
 
     return {
         bid,
         cardGroup,
-        typeAmounts,
-        playedType
+        typeAmounts,score
     }
 });
 
 const rankedCards = classifiedHands.sort((a,b) => {
-    if (a.playedType === b.playedType) {
-        return 1;
-    } else if (a.playedType < b.playedType) {
-        return 1;
-    } else {
-        return -1;
+    if(a.score === b.score) {
+        console.log(4444444, a, b)
     }
+    return a.score - b.score
 });
 
 
