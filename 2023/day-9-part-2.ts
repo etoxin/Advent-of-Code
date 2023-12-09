580233d7b67cf39ea01c04f839d22bf5e96c4168
Adam Lusted <94024260+adamlusted-wow@users.noreply.github.com>
2023-12-09 21:26:43 +1100

    stuff

--- 2023/day-9-part-2.ts
+++ 2023/day-9-part-2.ts
@@ -5,7 +5,7 @@ const _puzzleInput = `0 3 6 9 12 15
 10 13 16 21 30 45`;
 
 const lines = puzzleInput.split("\n").map((l) =>
-    l.split(" ").map((sn) => Number(sn))
+    l.split(" ").map((sn) => Number(sn)).reverse()
 );
 
 const isZero = (i: number) => i === 0;
