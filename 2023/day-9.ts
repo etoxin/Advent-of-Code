07d9d4d5cb91252408be62ab47c0df1f538813b9
Adam Lusted <94024260+adamlusted-wow@users.noreply.github.com>
2023-12-09 18:37:32 +1100

    stuff

--- 2023/day-9.ts
+++ 2023/day-9.ts
@@ -4,6 +4,9 @@ const _puzzleInput = `0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45`;
 
+const ___puzzleInput =
+  `15 18 30 59 111 187 296 499 999 2292 5394 12159 25703 50949 95308 169511 288607 473142 750534 1156659 1737663`;
+
 const lines = puzzleInput.split("\n").map((l) =>
   l.split(" ").map((sn) => Number(sn))
 );
@@ -11,9 +14,30 @@ const lines = puzzleInput.split("\n").map((l) =>
 const diff = (a: number, b: number) => Math.abs(a - b);
 const isZero = (i: number) => i === 0;
 
-const createDiff = (l: number[]) => {
+function findDifferenceSequence(sequence) {
+    let differences = [];
+    for(let i = 0; i < sequence.length - 1; i++) {
+        differences.push(sequence[i + 1] - sequence[i]);
+    }
+    return differences;
+}
+
+function isRootSequence(sequence: number[]) {
+    let isRoot = true;
+    for(let i = 0; i < sequence.length; i++) {
+        if (sequence[i] != 0) {
+            isRoot = false;
+            break;
+        }
+    }
+    return isRoot;
+}
+
+const createDiff = (l: number[]): number[] => {
+  // if (l.length === 1) return l;
+
   const diffArr = l.map((value, index, array) => {
-    if (index + 1 < array.length) {
+    if (index + 1 <= array.length) {
       return diff(value, array[index + 1]);
     }
   });
@@ -23,12 +47,14 @@ const createDiff = (l: number[]) => {
 
 const result = lines.map((l) => {
   const tree = [l];
-  while (!tree.at(-1).every(isZero)) {
-    tree.push(createDiff(tree.at(-1)));
+  while (!isRootSequence(tree.at(-1))) {
+    tree.push(findDifferenceSequence(tree.at(-1)));
   }
   return tree.map((l) => l.at(-1)).reverse().reduce((acc, cur) => {
     return acc + cur;
   }, 0);
 }).reduce((acc, cur) => acc + cur);
 
+
+// 1939607039
 console.log(result);
