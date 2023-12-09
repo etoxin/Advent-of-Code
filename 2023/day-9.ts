e343d80b97a9a7d3dcf299e7e3db0c49c442423b
Adam Lusted <94024260+adamlusted-wow@users.noreply.github.com>
2023-12-09 18:02:07 +1100

    stuff

--- 2023/day-9.ts
+++ 2023/day-9.ts
@@ -6,4 +6,27 @@ const lines = puzzleInput.split("\n").map((l) =>
   l.split(" ").map((sn) => Number(sn))
 );
 
-console.log(lines);
+const diff = (a: number, b: number) => Math.abs(a - b);
+const isZero = (i: number) => i === 0;
+
+const createDiff = (l: number[]) => {
+  const diffArr = l.map((value, index, array) => {
+    if (index + 1 < array.length) {
+      return diff(value, array[index + 1]);
+    }
+  });
+  diffArr.pop();
+  return diffArr;
+};
+
+const result = lines.map((l) => {
+  const tree = [l];
+  while (!tree.at(-1).every(isZero)) {
+    tree.push(createDiff(tree.at(-1)));
+  }
+  return tree.map((l) => l.at(-1)).reverse().reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+}).reduce((acc, cur) => acc + cur);
+
+console.log(result);
